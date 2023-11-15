88e9de6fdee15e74558234271cb23e0c01635b28
Gabriel Lagrotteria <gabriellagrotteria18@gmail.com>
2023-11-15 18:32:38 -0300

    se añadio bcrypt a utils

--- src/utils.js
+++ src/utils.js
@@ -1,11 +1,29 @@
-import { fileURLToPath } from 'url';
-import { dirname, join } from 'path';
+import {
+    fileURLToPath
+} from 'url';
+import {
+    dirname,
+    join
+} from 'path';
+import bcrypt from 'bcrypt';
 
-const __filename = fileURLToPath(import.meta.url);
+const __filename = fileURLToPath(
+    import.meta.url);
 const __dirname = dirname(__filename);
 
-export {__dirname};
+const createHash = password =>
+    bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+
+
+//Valida pass
+const isValidPassword = (plainPassword, hashedPassword) =>
+    bcrypt.compareSync(plainPassword, hashedPassword);
+
+export {
+    __dirname,
+    createHash,
+    isValidPassword
+};
 
 export const productsFilePath = join(__dirname, "./dao/fileManagers/files/productos.json")
 export const cartsFilePath = join(__dirname, "./dao/fileManagers/files/carrito.json");
-
